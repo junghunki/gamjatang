df5be683a42c9756086b517f716f0a4676244f60
GamJaTang <cho9856@gmail.com>
2018-11-05 12:57:01 -0800

    Create _589_n-ary-tree-preorder-traversal.java

---
+++ _589_n-ary-tree-preorder-traversal.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
+public class _589_n-ary-tree-preorder-traversal {
+   public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList();
+		dfs(res, root);
+		return res;
+    }
+	
+	private void dfs(List<Integer> res, Node root) {
+		if (root == null) return;
+		
+		res.add(root.val);
+		for (Node child : root.children) {
+			dfs(res, child);
+		}
+	}
+}
